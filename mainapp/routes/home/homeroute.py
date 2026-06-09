8b70c259a8ca7f4c10d279c2e8979ff788af3d84
Gabriel Swai <gabrielswai@game.local>
2026-06-09 13:33:50 -0400

    Archived old routes. Implemented a simple tone parser.

--- mainapp/routes/home/homeroute.py
+++ mainapp/routes/home/homeroute.py
@@ -1,4 +1,5 @@
 from flask import render_template, Blueprint, request
+from mainapp.services import tone_parser
 
 homeapp = Blueprint( 'homeroutes', __name__,
                       static_folder='static',
@@ -8,6 +9,22 @@ homeapp = Blueprint( 'homeroutes', __name__,
 def home():
     return render_template('home.html')
 
-@homeapp.route('/worlds-best-mom')
-def test():
-    return render_template('worlds-best-mom.html')
+# @homeapp.route('/worlds-best-mom')
+# def mom():
+#     return render_template('worlds-best-mom.html')
+
+@homeapp.route('/tone-parser', methods=['GET', 'POST'])
+def extract_tones():
+    raw_phrases = request.form.get("phrases", "")
+    default_tone = request.form.get("default_tone", "L")
+    results = tone_parser.parse_phrases(
+        raw_phrases=raw_phrases,
+        default_tone=default_tone,
+    )
+
+    return render_template(
+        "tone-parser.html",
+        results=results,
+        original_input=raw_phrases,
+        default_tone=default_tone
+    )
---
+++ mainapp/services/tone_parser.py
@@ -0,0 +1,105 @@
+# =====================================================================================
+# Last modification: 2026/06/09
+#
+# This script automatically parses tone sequences from phrases with diacritics.
+# =====================================================================================
+
+
+# Function:     parse_phrases
+# Inputs:       raw_phrases | multiline input phrases (str)
+#               default_tone | default tone for vowels with no diacritic (str)
+# Outputs:      parsed_phrases | list of annotation intervals and labels from the specified tier (list[dict])
+# Description:  Separates a multiline string of phrases by phrase and parses them with parse_tones().
+
+def parse_phrases(raw_phrases, default_tone):
+    phrases = [
+        line.strip()
+        for line in raw_phrases.splitlines()
+        if line.strip()
+    ]
+
+    parsed_phrases = []
+
+    for phrase in phrases:
+        tone_sequence = parse_tones(
+            phrase=phrase,
+            default_tone=default_tone
+        )
+
+        parsed_phrases.append({
+            "phrase": phrase,
+            "tone_sequence": tone_sequence,
+        })
+
+    return parsed_phrases
+
+
+# Function:     parse_tones
+# Inputs:       phrase | input phrases (str)
+#               default_tone | default tone for vowels with no diacritic (str)
+# Outputs:      tones | parsed tone sequence (str)
+# Description:  Takes an input phrase with diacritics and produces the surface tone pattern.
+
+def parse_tones(phrase, default_tone):
+    DEFAULT_VOWEL_LIST = ["i", "ɨ", "ʉ", "ɯ", "u", "ɪ", "ʏ", "ʊ", "e", "ø", "ɘ", "ɵ", "ɤ", "o", "ə", "ɛ", "œ", "ɜ", "ɞ", "ʌ", "ɔ", "æ", "ɐ", "a", "ɶ", "ä", "ɑ", "ɒ",
+                          "ọ", "ẹ"]
+    H_VOWEL_LIST = ["á", "é", "í", "ó", "ú"]
+    L_VOWEL_LIST = ["à", "è", "ì", "ò", "ù"]
+    F_VOWEL_LIST = ["â", "ê", "î", "ô", "û", "ȃ", "ȇ", "ȋ", "ȏ", "ȗ"]
+    R_VOWEL_LIST = ["ǎ", "ě", "ǐ", "ǒ", "ǔ", "ă", "ĕ", "ĭ", "ŏ", "ŭ"]
+
+    tones = ""
+
+    for i in range(len(phrase.lower())):
+        char = phrase[i]
+
+        if char == " ":
+            tones = tones[0:len(tones)-1]
+            tones += " "
+
+        for vowel in H_VOWEL_LIST:
+            if char == vowel:
+                tones += "H-"
+
+        for vowel in L_VOWEL_LIST:
+            if char == vowel:
+                tones += "L-"
+
+        for vowel in F_VOWEL_LIST:  
+            if char == vowel:
+                tones += "HL-"
+
+        for vowel in R_VOWEL_LIST:  
+            if char == vowel:
+                tones += "LH-"
+
+        if i+1 < len(phrase):
+            next_char = phrase[i+1]
+        
+        else:
+            continue
+        
+        if next_char == "́":
+            tones += "H-"
+        
+        elif next_char == "̀":
+            tones += "L-"
+        
+        elif next_char == "̄":
+            tones += "M-"
+        
+        elif next_char == "̂":
+            tones += "HL-"
+        
+        elif next_char == "̌":
+            tones += "LH-"
+        
+        else:
+            for vowel in DEFAULT_VOWEL_LIST:
+                if char == vowel:
+                    tones += f"{default_tone}-"
+
+
+    tones = tones[0:len(tones)-1]
+
+    return tones
